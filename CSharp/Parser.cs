9cd78e213cf457c3714af35f84f127ce59ca1e3b
Colin van Loo <colin@vanloo.ch>
2025-10-07 02:26:06 +0200

    c#: wip: apply intellisense suggestions where they made sense

--- CSharp/Parser.cs
+++ CSharp/Parser.cs
@@ -50,54 +50,49 @@ EndOfFile <- !.
 
 using System.Diagnostics.CodeAnalysis;
 
-public sealed class Parser {
+public sealed class Parser(string input)
+{
     // @todo: make this parser generate a packrat parser out of a PEG
     //    -> source generators?
 
-    private Tokenizer _tokenizer;
+    readonly Tokenizer _tokenizer = new(input);
 
-    private char Next() {
+    char Next() {
         return _tokenizer.Next();
     }
-    private bool TryNext(out char? next) {
+    bool TryNext([NotNullWhen(true)] out char? next) {
         return _tokenizer.TryNext(out next);
     }
-    private char Peek() {
+    char Peek() {
         return _tokenizer.Peek();
     }
-    private bool TryPeek(out char? peek) {
+    bool TryPeek([NotNullWhen(true)] out char? peek) {
         return _tokenizer.TryPeek(out peek);
     }
-    private ReadOnlySpan<char> NextString(int amount) {
+    ReadOnlySpan<char> NextString(int amount) {
         return _tokenizer.NextString(amount);
     }
-    private bool TryNextString(int amount, out string? next) {
+    bool TryNextString(int amount, [NotNullWhen(true)] out string? next) {
         return _tokenizer.TryNextString(amount, out next);
     }
-    private ReadOnlySpan<char> PeekString(int amount) {
+    ReadOnlySpan<char> PeekString(int amount) {
         return _tokenizer.PeekString(amount);
     }
-    private bool TryPeekString(int amount, out string? peek) {
+    bool TryPeekString(int amount, [NotNullWhen(true)] out string? peek) {
         return _tokenizer.TryPeekString(amount, out peek);
     }
-    private bool AtEnd => _tokenizer.AtEnd;
-    //private Tokenizer.SavedPosition Mark => _tokenizer.Mark;
-    //private void Reset(Tokenizer.SavedPosition pos) => _tokenizer.Reset(pos);
-    private Tokenizer.SavedPosition Mark() {
+    bool AtEnd => _tokenizer.AtEnd;
+    Tokenizer.SavedPosition Mark() {
         return _tokenizer.Mark();
     }
-    private void Reset(Tokenizer.SavedPosition pos) {
+    void Reset(Tokenizer.SavedPosition pos) {
         _tokenizer.Reset(pos);
     }
-    private bool IfMatch(string match) {
+    bool IfMatch(string match) {
         return TryPeekString(match.Length, out var p) && p == match;
     }
-    private bool IfMatchSkip(string match) {
+    bool IfMatchSkip(string match) {
         return TryNextString(match.Length, out var n) && n == match;
-    }
-
-    public Parser(string input) {
-        _tokenizer = new Tokenizer(input);
     }
 
     public bool TryParse([NotNullWhen(true)] out IEnumerable<Rule>? result) {
@@ -120,7 +115,7 @@ public sealed class Parser {
             }
         }
         Reset(pos);
-        result = default;
+        result = null;
         return false;
     }
 
@@ -128,7 +123,7 @@ public sealed class Parser {
     bool ParseDefinition([NotNullWhen(true)] out Rule? result) {
         var pos = Mark();
         if (ParseIdentifier(out var ident)) {
-            if (ParseTerminal("<-", out _)) {
+            if (MatchAndSkip("<-")) {
                 if (ParseExpression(out var exp)) {
                     result = new Rule(ident, exp);
                     return true;
@@ -136,8 +131,8 @@ public sealed class Parser {
             }
         }
         Reset(pos);
-        result = default;
-        return default;
+        result = null;
+        return false;
     }
 
     // Identifier <- IdentStart IdentCont* Spacing
@@ -153,30 +148,29 @@ public sealed class Parser {
                 return true;
             }
         }
-        result = default;
-        return default;
+        Reset(pos);
+        result = null;
+        return false;
     }
 
     // IdentStart <- [a-zA-Z_]
     bool ParseIdentStart([NotNullWhen(true)] out char? result) {
         if (TryPeek(out var c) && ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || c == '_')) {
-        //if (Peek() is var c && ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || c == '_')) {
             result = Next();
             return true;
         }
-        result = default;
-        return default;
+        result = null;
+        return false;
     }
 
     // IdentCont <- IdentStart / [0-9]
     bool ParseIdentCont([NotNullWhen(true)] out char? result) {
         if (TryPeek(out var c) && ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9') || c == '_')) {
-        //if (Peek() is var c && ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9') || c == '_')) {
             result = Next();
             return true;
         }
-        result = default;
-        return default;
+        result = null;
+        return false;
     }
 
     // LEFTARROW  <- '<-' Spacing
@@ -189,33 +183,31 @@ public sealed class Parser {
     // OPEN       <- '(' Spacing
     // CLOSE      <- ')' Spacing
     // DOT        <- '.' Spacing
-    bool ParseTerminal(string terminal, [NotNullWhen(true)] out string? result) {
+    bool MatchAndSkip(string literal) {
         var pos = Mark();
-        if (IfMatchSkip(terminal)) {
+        if (IfMatchSkip(literal)) {
             if (ParseSpacing(out _)) {
-                result = terminal;
                 return true;
             }
         }
         Reset(pos);
-        result = default;
-        return default;
+        return false;
     }
 
     // Expression <- Sequence (SLASH Sequence)*
     bool ParseExpression([NotNullWhen(true)] out Expression? result) {
         var pos = Mark();
         if (ParseSequence(out var seq1)) {
-            var sequence = new List<Expression>([(Expression)seq1]);
-            while (ParseTerminal("/", out _) && ParseSequence(out var seq)) {
+            var sequence = new List<Expression>([seq1]);
+            while (MatchAndSkip("/") && ParseSequence(out var seq)) {
                 sequence.Add(seq);
             }
             result = new PrioritizedChoice(sequence);
             return true;
         }
         Reset(pos);
-        result = default;
-        return default;
+        result = null;
+        return false;
     }
 
     // Sequence <- Prefix*
@@ -231,14 +223,14 @@ public sealed class Parser {
     // Prefix <- (AND / NOT)? Suffix
     bool ParsePrefix([NotNullWhen(true)] out Expression? result) {
         var pos = Mark();
-        if (ParseTerminal("&", out _)) {
+        if (MatchAndSkip("&")) {
             if (ParseSuffix(out var andSuffix)) {
                 result = new AndPredicate(andSuffix);
                 return true;
             }
         }
         Reset(pos);
-        if (ParseTerminal("!", out _)) {
+        if (MatchAndSkip("!")) {
             if (ParseSuffix(out var notSuffix)) {
                 result = new NotPredicate(notSuffix);
                 return true;
@@ -250,31 +242,31 @@ public sealed class Parser {
             return true;
         }
         Reset(pos);
-        result = default;
-        return default;
+        result = null;
+        return false;
     }
 
     // Suffix <- Primary (QUESTION / STAR / PLUS)?
     bool ParseSuffix([NotNullWhen(true)] out Expression? result) {
-        // result might be just the primary, or the primary rapped in a Option, ZeroOrMore, or OneOrMore
+        // result might be just the primary, or the primary rapped in an Option, ZeroOrMore, or OneOrMore
         if (ParsePrimary(out var primary)) {
-            if (ParseTerminal("?", out _)) {
+            if (MatchAndSkip("?")) {
                 result = new Option(primary);
                 return true;
             }
-            if (ParseTerminal("*", out _)) {
+            if (MatchAndSkip("*")) {
                 result = new ZeroOrMore(primary);
                 return true;
             }
-            if (ParseTerminal("+", out _)) {
+            if (MatchAndSkip("+")) {
                 result = new OneOrMore(primary);
                 return true;
             }
             result = primary;
             return true;
         }
-        result = default;
-        return default;
+        result = null;
+        return false;
     }
 
     // Primary <- Identifier !LEFTARROW
@@ -283,15 +275,15 @@ public sealed class Parser {
     bool ParsePrimary([NotNullWhen(true)] out Expression? result) {
         var pos = Mark();
         if (ParseIdentifier(out var ident)) {
-            if (!ParseTerminal("<-", out _)) {
+            if (!MatchAndSkip("<-")) {
                 result = new NonTerminal(ident);
                 return true;
             }
         }
         Reset(pos);
-        if (ParseTerminal("(", out _)) {
+        if (MatchAndSkip("(")) {
             if (ParseExpression(out var exp)) {
-                if (ParseTerminal(")", out _)) {
+                if (MatchAndSkip(")")) {
                     result = exp;
                     return true;
                 }
@@ -306,13 +298,13 @@ public sealed class Parser {
             result = new CharacterClass(klass);
             return true;
         }
-        if (ParseTerminal(".", out _)) {
+        if (MatchAndSkip(".")) {
             result = new AnyTerminal();
             return true;
         }
         Reset(pos);
-        result = default;
-        return default;
+        result = null;
+        return false;
     }
 
     // Literal <- ['] (!['] Char)* ['] Spacing
@@ -345,8 +337,8 @@ public sealed class Parser {
             }
         }
         Reset(pos);
-        result = default;
-        return default;
+        result = null;
+        return false;
     }
 
     // Class <- '[' (!']' Range)* ']' Spacing
@@ -365,8 +357,8 @@ public sealed class Parser {
             }
         }
         Reset(pos);
-        result = default;
-        return default;
+        result = null;
+        return false;
     }
 
     // Char <- '\\' [nrt'"\[\]\\]
@@ -434,8 +426,8 @@ public sealed class Parser {
             result = Next();
             return true;
         }
-        result = default;
-        return default;
+        result = null;
+        return false;
     }
 
     // Range <- Char '-' Char / Char
@@ -455,8 +447,8 @@ public sealed class Parser {
             return true;
         }
         Reset(pos);
-        result = default;
-        return default;
+        result = null;
+        return false;
     }
 
     // Spacing <- (Space / Comment)*
@@ -478,8 +470,8 @@ public sealed class Parser {
             result = lineEnd;
             return true;
         }
-        result = default;
-        return default;
+        result = null;
+        return false;
     }
 
     // Comment <- '#' (!EndOfLine .)* EndOfLine
@@ -496,8 +488,8 @@ public sealed class Parser {
             }
         }
         Reset(pos);
-        result = default;
-        return default;
+        result = null;
+        return false;
     }
 
     // EndOfLine <- '\r\n' / '\n' / '\r'
@@ -510,8 +502,8 @@ public sealed class Parser {
             result = "" + Next();
             return true;
         }
-        result = default;
-        return default;
+        result = null;
+        return false;
     }
 
     delegate bool ParserFunc<T>(out T? result);
@@ -526,18 +518,13 @@ public sealed class Parser {
     }
 }
 
-public sealed class Tokenizer {
-    private readonly string _input;
-
+public sealed class Tokenizer(string input)
+{
     public int Position { get; private set; }
 
-    public Tokenizer(string input) {
-        _input = input;
-    }
-
-    public bool AtEnd => _input.Length == Position;
+    public bool AtEnd => input.Length == Position;
     public char Next() {
-        return _input[Position++];
+        return input[Position++];
     }
     public bool TryNext([NotNullWhen(true)] out char? next) {
         if (!AtEnd) {
@@ -548,7 +535,7 @@ public sealed class Tokenizer {
         return false;
     }
     public char Peek() {
-        return _input[Position];
+        return input[Position];
     }
     public bool TryPeek([NotNullWhen(true)] out char? peek) {
         if (!AtEnd) {
@@ -560,12 +547,12 @@ public sealed class Tokenizer {
     }
 
     public ReadOnlySpan<char> NextString(int amount) {
-        var span = _input.AsSpan().Slice(start: Position, length: amount);
+        var span = input.AsSpan().Slice(start: Position, length: amount);
         Position += amount;
         return span;
     }
     public bool TryNextString(int amount, [NotNullWhen(true)] out string? next) {
-        if (Position < _input.Length - amount) {
+        if (Position < input.Length - amount) {
             next = NextString(amount).ToString();
             return true;
         }
@@ -574,11 +561,11 @@ public sealed class Tokenizer {
     }
 
     public ReadOnlySpan<char> PeekString(int amount) {
-        var span = _input.AsSpan().Slice(start: Position, length: amount);
+        var span = input.AsSpan().Slice(start: Position, length: amount);
         return span;
     }
     public bool TryPeekString(int amount, [NotNullWhen(true)] out string? peek) {
-        if (Position < _input.Length - amount) {
+        if (Position < input.Length - amount) {
             peek = PeekString(amount).ToString();
             return true;
         }
@@ -597,7 +584,7 @@ public sealed class Tokenizer {
 
     public class SavedPosition {
         public int Position { get; private init; }
-        private SavedPosition() {}
+
         internal SavedPosition(int pos) {
             Position = pos;
         }
@@ -607,7 +594,7 @@ public sealed class Tokenizer {
     }
 
     public string Rest() {
-        return _input.Substring(Position);
+        return input.Substring(Position);
     }
 }
 
@@ -619,49 +606,49 @@ public readonly record struct Rule(string Name, Expression Expression) {
 
 // @todo: Visitor Pattern
 // @todo: Fix pretty printing
-public abstract record class Expression;
+public abstract record Expression;
 
-public sealed record class Terminal(string Symbol) : Expression {
+public sealed record Terminal(string Symbol) : Expression {
     public override string ToString() {
         return $"'{Symbol}'";
     }
 }
 
-public sealed record class NonTerminal(string Name): Expression {
+public sealed record NonTerminal(string Name): Expression {
     public override string ToString() {
         return Name;
     }
 }
 
-public sealed record class Sequence(List<Expression> Expressions) : Expression {
+public sealed record Sequence(List<Expression> Expressions) : Expression {
     public override string ToString() {
         return Expressions.Aggregate("", (s, exp) => {
             if (s == "") {
                 return exp.ToString();
             }
-            return s + " " + exp.ToString();
+            return s + " " + exp;
         });
     }
 }
 
-public sealed record class PrioritizedChoice(List<Expression> Choices) : Expression {
+public sealed record PrioritizedChoice(List<Expression> Choices) : Expression {
     public override string ToString() {
         return Choices.Aggregate("", (s, exp) => {
             if (s == "") {
                 return exp.ToString();
             }
-            return s + " / " + exp.ToString();
+            return s + " / " + exp;
         });
     }
 }
 
-public sealed record class ZeroOrMore(Expression Inner) : Expression {
+public sealed record ZeroOrMore(Expression Inner) : Expression {
     public override string ToString() {
         return $"{Inner}*";
     }
 }
 
-public sealed record class NotPredicate(Expression Inner) : Expression {
+public sealed record NotPredicate(Expression Inner) : Expression {
     public override string ToString() {
         return $"!{Inner}";
     }
@@ -669,31 +656,31 @@ public sealed record class NotPredicate(Expression Inner) : Expression {
 
 // Sugar
 
-public sealed record class AnyTerminal : Expression {
+public sealed record AnyTerminal : Expression {
     public override string ToString() {
         return ".";
     }
 }
 
-public sealed record class CharacterClass(CharSet Klass) : Expression {
+public sealed record CharacterClass(CharSet Klass) : Expression {
     public override string ToString() {
         return $"[{Klass}]";
     }
 }
 
-public sealed record class Option(Expression Inner) : Expression {
+public sealed record Option(Expression Inner) : Expression {
     public override string ToString() {
         return $"{Inner}?";
     }
 }
 
-public sealed record class OneOrMore(Expression Inner) : Expression {
+public sealed record OneOrMore(Expression Inner) : Expression {
     public override string ToString() {
         return $"{Inner}+";
     }
 }
 
-public sealed record class AndPredicate(Expression Inner) : Expression {
+public sealed record AndPredicate(Expression Inner) : Expression {
     public override string ToString() {
         return $"&{Inner}";
     }
@@ -720,6 +707,6 @@ public readonly record struct Ranges(IEnumerable<CharSet> Sets) : CharSet {
         return Sets.FirstOrDefault(set => set.Contains(c)) != null;
     }
     public override string ToString() {
-        return Sets.Aggregate("", (s, range) => s + range.ToString());
+        return Sets.Aggregate("", (s, range) => s + range);
     }
 }
